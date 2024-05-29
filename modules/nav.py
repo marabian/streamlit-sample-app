e809faac5987c7b1f9990458c314d790cc066891
Mher <mherarabian@gmail.com>
2024-05-29 13:39:08 -0700

    changes

--- modules/nav.py
+++ modules/nav.py
@@ -6,8 +6,10 @@
 # Notice that we set `showSidebarNavigation = false` in config.toml to do this
 
 import streamlit as st
+
 # add add_logo from streamlit extras
 # from streamlit_extras.app_logo import add_logo
+
 
 def add_logo():
     st.markdown(
@@ -32,28 +34,29 @@ def add_logo():
         unsafe_allow_html=True,
     )
 
+
 def Navbar():
     # builds the sidebar menu
     with st.sidebar:
         # add app logo
         # add_logo()
-        st.image("logo/golden_hippo_logo_test.png")
+        # st.image("logo/golden_hippo_logo_test.png")
 
         # https://docs.streamlit.io/develop/api-reference/widgets/st.page_link
-        st.page_link('Hello.py', label='Hello', icon='📄')
-        st.page_link('pages/1_Widgets.py', label='Widgets', icon='🕹')
+        st.page_link("Hello.py", label="Hello", icon="📄")
+        st.page_link("pages/1_Widgets.py", label="Widgets", icon="🕹")
         # st.page_link('pages/2_Plotting.py', label='Plotting', icon='📊')
-        st.page_link('pages/3_Miscellaneous.py', label='Miscellaneous', icon='📦')
-        st.page_link('pages/4_Layout.py', label='Layout', icon='📦')
-        st.page_link('pages/5_Styling.py', label='Styling', icon='🎨')
+        st.page_link("pages/3_Miscellaneous.py", label="Miscellaneous", icon="📦")
+        st.page_link("pages/4_Layout.py", label="Layout", icon="📦")
+        st.page_link("pages/5_Styling.py", label="Styling", icon="🎨")
         # st.page_link('pages/6_Extras.py', label='Extras', icon='🎉')
-        st.page_link('pages/7_Test.py', label='Test', icon='🧪')
-        st.page_link('pages/8_Login.py', label='Login', icon='🔒')
-        st.page_link('pages/9_Register.py', label='Register', icon='🔒')
-        st.markdown('---')
+        st.page_link("pages/7_Test.py", label="Test", icon="🧪")
+        st.page_link("pages/8_Login.py", label="Login", icon="🔒")
+        st.page_link("pages/9_Register.py", label="Register", icon="🔒")
+        st.markdown("---")
 
         # COMMENT OUT THIS PART AFTER DEVELOPMENT
-        st.markdown('### To-do')
+        st.markdown("### To-do")
         st.markdown(
             """
             - [x] Finish Widgets page
@@ -67,7 +70,7 @@ def Navbar():
         st.text("")
 
         st.markdown("### Session State")
-        st.markdown('---')
+        st.markdown("---")
         st.session_state
         # END OF COMMENT
 
